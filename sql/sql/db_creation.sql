8e14c74e6732495e717f518b7a80e1808ba2a28b
Kay Hiratsuka <77436163+kaaayeyey@users.noreply.github.com>
2022-02-03 13:42:02 -0800

    db_creation.sql
     SQL code to create a database

---
+++ sql/sql/db_creation.sql
@@ -0,0 +1,12 @@
+-- Database: Ekata_SQLExercise
+
+-- DROP DATABASE IF EXISTS "Ekata_SQLExercise";
+
+CREATE DATABASE "Ekata_SQLExercise"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
